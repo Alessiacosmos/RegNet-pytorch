479548ab1a3437bdc746ca217626bbdb5984bb12
Alessiacosmos <35725366+Alessiacosmos@users.noreply.github.com>
2020-04-19 18:56:37 +0800

    Add files via upload

--- data/__init__.py
+++ data/__init__.py
@@ -1,1 +1,7 @@
-
+# -*- encoding: utf-8 -*-
+"""
+@File    : __init__.py
+@Time    : 2020/4/13 14:38
+@Author  : Alessia K
+@Email   : ------
+"""
---
+++ data/dict_class.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+"""
+@File    : dict_class.py
+@Time    : 2020/4/12 15:03
+@Author  : Alessia K
+@Email   : ------
+"""
+
+class_name = {
+    'aeroplane': 0,
+    'bicycle': 1,
+    'bird': 2,
+    'boat': 3,
+    'bottle': 4,
+    'but': 5
+}
